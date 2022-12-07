19e73a4b0ed4b2fec6d843d97ab597aca911e9a5
Brandon_Robison <bjrobis@gmail.com>
2022-12-07 13:21:13 -0700

    changed code

--- oop_game-v2/js/Game.js
+++ oop_game-v2/js/Game.js
@@ -1,3 +1,24 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * Game.js */
+
+class Game {
+    constructor(phrase) {
+        this.missed = 0;
+        this.phrases = ['death by a thousand cuts', 
+                        'meet me in the afterglow', 
+                        'familiarity breeds contempt',
+                        'meet me at midnight',
+                        'i remember it all too well'];
+        this.activePhrase = null;
+    }
+/**
+ * Selects random phrase from phrases property
+ * @return {Object} Phrase object chosen to be used
+ */
+    getRandomPhrase() {
+        let randomPhrase = this.phrases[Math.floor(Math.random() * this.phrases.length)];
+        return randomPhrase;
+        this.activePhrase = randomPhrase;
+    }
+}
--- oop_game-v2/js/Phrase.js
+++ oop_game-v2/js/Phrase.js
@@ -1,3 +1,20 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * Phrase.js */
+let phraseBox = document.getElementById('phrase');
+
+
+class Phrase {
+    constructor(phrase) {
+        this.phrase = phrase.toLowerCase();
+    }
+
+    addPhraseToDisplay() {
+    let displayArray = this.phrase.split('');
+    console.log(displayArray);
+        
+
+    }
+
+}
+
